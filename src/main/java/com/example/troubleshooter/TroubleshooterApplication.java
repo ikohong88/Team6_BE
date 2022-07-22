28390e75b79f2d5f5d9c4434fa2fa88bda86f99c
KimHyeon <hye07on11@gmail.com>
2022-07-22 14:39:44 +0900

    Initial commit

---
+++ src/main/java/com/example/troubleshooter/TroubleshooterApplication.java
@@ -0,0 +1,13 @@
+package com.example.troubleshooter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TroubleshooterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TroubleshooterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/troubleshooter/TroubleshooterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.troubleshooter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TroubleshooterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
