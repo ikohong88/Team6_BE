33935302cadffc9426d868433739fe910ca16075
ikohong <ikohong88@gmail.com>
2022-07-22 20:47:50 +0900

    - 22.07.22 security 및 로그인 기능 구현

--- src/main/java/com/example/troubleshooter/security/WebSecurityConfig.java
+++ src/main/java/com/example/troubleshooter/security/WebSecurityConfig.java
@@ -2,44 +2,61 @@ package com.example.troubleshooter.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-
 @Configuration
 @EnableWebSecurity // 스프링 Security 지원을 가능하게 함
+@EnableGlobalMethodSecurity(securedEnabled = true) // @Secured 어노테이션 활성화
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    
-    // 패스워드 암호화 구현
-    // 'BCrypt 해시함수'를 사용해 패스워드를 암호화하여 DB 에 저장
+
     @Bean
     public BCryptPasswordEncoder encodePassword() {
         return new BCryptPasswordEncoder();
     }
 
     @Override
+    public void configure(WebSecurity web) {
+        // h2-console 사용에 대한 허용 (CSRF, FrameOptions 무시)
+        web
+                .ignoring()
+                .antMatchers("/h2-console/**");
+    }
+
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // 회원 관리 처리 API (POST /user/**) 에 대해 CSRF 무시
-        http.csrf()
-                .ignoringAntMatchers("/user/**");
+        http.csrf().disable();
 
         http.authorizeRequests()
+                // 회원 관리 처리 API 전부를 login 없이 허용
                 .antMatchers("/api/**").permitAll()
-                // 어떤 요청이든 '인증'
+                // 그 외 어떤 요청이든 '인증'
                 .anyRequest().authenticated()
             .and()
-                // 로그인 기능 허용
+                // [로그인 기능]
                 .formLogin()
-
+                // 로그인 View 제공 (GET /user/login)
 //                .loginPage("/user/login")
+                // 로그인 처리 (POST /user/login)
+                .loginProcessingUrl("/api/login")
+                // 로그인 처리 후 성공 시 URL
                 .defaultSuccessUrl("/")
-                .failureUrl("/user/login?error")
+                // 로그인 처리 후 실패 시 URL
+//                .failureUrl("/user/login?error")
                 .permitAll()
             .and()
-                // 로그아웃 기능 허용
+                // [로그아웃 기능]
                 .logout()
-                .permitAll();
+                // 로그아웃 요청 처리 URL
+                .logoutUrl("/api/logout")
+                .permitAll()
+            .and()
+                .exceptionHandling();
+                // "접근 불가" 페이지 URL 설정
+//                .accessDeniedPage("/forbidden.html");
     }
 }
