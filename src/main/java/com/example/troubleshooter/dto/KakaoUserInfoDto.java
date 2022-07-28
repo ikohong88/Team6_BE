62cb741683ad4479e0faa915f9b227102467f134
ju-ei8ht <qkr8125@gmail.com>
2022-07-28 21:37:33 +0900

    카카오 로그인 기능 추가

---
+++ src/main/java/com/example/troubleshooter/dto/KakaoUserInfoDto.java
@@ -0,0 +1,11 @@
+package com.example.troubleshooter.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class KakaoUserInfoDto {
+    private String id;
+//    private String nickname;
+}
